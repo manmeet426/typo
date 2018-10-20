84752c14ab7cd2a27f869d0af9815a770d75f127
EC2 Default User <ec2-user@ip-172-31-4-202.us-east-2.compute.internal>
2018-10-20 02:19:28 +0000

    Cucumber scenario added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -26,7 +26,9 @@ class Admin::CategoriesController < Admin::BaseController
   def new_or_edit
     @categories = Category.find(:all)
     @category = if  params[:id] == nil then Category.new  else Category.find(params[:id]) end
+    # added if conditon to handle empty parametners
     # @category = Category.find(params[:id])
+    
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
